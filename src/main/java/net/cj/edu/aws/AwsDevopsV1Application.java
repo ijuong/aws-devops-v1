2cac74c9d1b47f6a44ce40b7761475d17f3d72e6
ijuyong <juyong.lee@cj.net>
2022-11-09 11:17:40 +0900

    first commit

---
+++ src/main/java/net/cj/edu/aws/AwsDevopsV1Application.java
@@ -0,0 +1,13 @@
+package net.cj.edu.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDevopsV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsDevopsV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/net/cj/edu/aws/AwsDevopsV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.cj.edu.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDevopsV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
